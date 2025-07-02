cbbfa698be126c5ab207e71a327eec1ed6810fb4
Fredy-Angarita <frediangarita1999@gmail.com>
2025-07-02 16:04:11 -0500

    fix (contact): remove unnecessary contact-info div and adjust styles for better layout

--- components/contact-container.vue
+++ components/contact-container.vue
@@ -7,8 +7,6 @@
       <ContactBadge icon="streamline-color:new-file-flat" label="Ver CV" />
       <ContactBadge icon="devicon:linkedin" label="Linkedin" />
       <ContactBadge icon="skill-icons:github-dark" label="Github" />
-    </div>
-    <div class="contact-info">
       <ContactBadge
         icon="streamline-ultimate-color:phone-circle"
         label="Teléfono"
@@ -25,19 +23,27 @@
   height: 100%;
   overflow-y: auto;
   scrollbar-width: none;
+  border-radius: 0.4rem;
   padding: 0.5rem;
-  gap: 1rem;
+  gap: 1.2rem;
+
   &-title {
     text-align: center;
     font-size: 1.5rem;
   }
-  &-networks,
-  &-info {
+  &-networks {
     display: flex;
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
     gap: 0.5rem;
+  }
+}
+@media (max-width: 1350px) {
+  .contact {
+    -webkit-box-shadow: inset 0px -32px 18px -29px rgba(66, 68, 90, 1);
+    -moz-box-shadow: inset 0px -32px 18px -29px rgba(66, 68, 90, 1);
+    box-shadow: inset 0px -32px 18px -29px rgba(66, 68, 90, 1);
   }
 }
 </style>
